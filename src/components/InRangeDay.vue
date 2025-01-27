70a2e881452182e9b8d11dea65703b5999efca39
Andrea Burattin <delas@users.noreply.github.com>
2025-01-27 15:27:41 +0100

    Update InRangeDay.vue

--- src/components/InRangeDay.vue
+++ src/components/InRangeDay.vue
@@ -1,5 +1,5 @@
 <template>
-	<span v-if="valueOnly">{{ inRange }}% in range <small>[{{ max }}%, {{ min }}%]</small></span>
+	<span v-if="valueOnly">{{ inRange }}% in range <small>[{{ min }}%, {{ max }}%]</small></span>
 	<div v-else class="text-center">
 		<p class="muted mt-4 float-left">In range</p>
 		<v-progress-circular v-if="size > 0" :model-value="inRange" :size="size" :width="size / 10">
